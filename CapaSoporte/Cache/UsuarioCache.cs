9f4572b9a5ac0e23745bd54c78f34b91cc1d8a5a
José Luis Arroyo Núñez <51970076+DecodeLuis001@users.noreply.github.com>
2022-12-21 12:29:08 -0600

    Add files via upload

---
+++ CapaSoporte/Cache/UsuarioCache.cs
@@ -0,0 +1,15 @@
+﻿namespace CapaSoporte.Cache
+{
+    //Se incertan los datos de tu tabla en SQL.
+    public static class UsuarioCache
+    {
+        public static int IDUsuario { get; set; }
+        public static int IDTipoUsuario { get; set; }
+        public static string Nombre { get; set; }
+        public static string ApellidoPaterno { get; set; }
+        public static string ApellidoMaterno { get; set; }
+        public static string Usuario { get; set; }
+        public static string Contraseña { get; set; }
+        public static bool Activo { get; set; }
+    }
+}
